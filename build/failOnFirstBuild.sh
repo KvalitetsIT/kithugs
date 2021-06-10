1f0d809bf4adab83301f67c0a2a2812df28a7d53
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-06-10 11:25:19 +0200

    Fix issue when used as template and first build.

--- build/failOnFirstBuild.sh
+++ build/failOnFirstBuild.sh
@@ -2,7 +2,7 @@
 
 echo "${GITHUB_REPOSITORY}"
 echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "kvalitetsit/kithugs" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/kithugs" ]; then
+if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/kithugs" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/kithugs" ]; then
   echo "Please run setup.sh"
   exit 1
 fi
