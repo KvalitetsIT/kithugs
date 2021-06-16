41b9209b83a6f32b6c7fbe04da3a411bff60fe10
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-06-16 11:29:13 +0200

    Input validation in setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z $1 ]; then
+  echo "Run setup.sh REPOSITORY_NAME"
+  exit 1
+fi
+
 ignore_files=".git|setup.sh"
 
 for input_file in `tree -I "${ignore_files}" -Ffai --noreport`
