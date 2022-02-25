cff19c141a7973fba3bfc74f12d885e772589145
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-02-25 11:01:42 +0100

    Make compose setup work and service callable through Swagger UI.

--- service/src/main/java/dk/kvalitetsit/hello/controller/HelloController.java
+++ service/src/main/java/dk/kvalitetsit/hello/controller/HelloController.java
@@ -6,9 +6,12 @@ import org.openapitools.api.KithugsApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// CORS - Consider if this is needed in your application. Only here to make Swagger UI work.
+@CrossOrigin(origins = "http://localhost")
 public class HelloController implements KithugsApi {
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     private final HelloService helloService;
