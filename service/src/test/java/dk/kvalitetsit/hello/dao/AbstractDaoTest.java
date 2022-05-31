8dc376bc829bf4e584aa8925435aa45f43510037
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-05-31 11:00:15 +0200

    Migrate from mySQL to MariaDB.

--- service/src/test/java/dk/kvalitetsit/hello/dao/AbstractDaoTest.java
+++ service/src/test/java/dk/kvalitetsit/hello/dao/AbstractDaoTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.MariaDBContainer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @PropertySource("test.properties")
@@ -21,18 +21,18 @@ abstract public class AbstractDaoTest {
     private static Object initialized = null;
 
     @BeforeClass
-    public static void setupMySqlJdbcUrl() {
+    public static void setupMariadbJdbcUrl() {
         if (initialized == null) {
             var username = "hellouser";
             var password = "secret1234";
 
-            MySQLContainer mysql = new MySQLContainer("mysql:5.7")
+            MariaDBContainer mariadb = new MariaDBContainer("mariadb:10.6")
                     .withDatabaseName("hellodb")
                     .withUsername(username)
                     .withPassword(password);
-            mysql.start();
+            mariadb.start();
 
-            String jdbcUrl = mysql.getJdbcUrl();
+            String jdbcUrl = mariadb.getJdbcUrl();
             System.setProperty("jdbc.url", jdbcUrl);
             System.setProperty("jdbc.user", username);
             System.setProperty("jdbc.pass", password);
