791a1065d02308d29ac19cc87753880b65eaeac5
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-11-23 12:37:44 +0100

    Rename class and minor code cleanup.

--- web/src/main/java/dk/kvalitetsit/hello/Application.java
+++ web/src/main/java/dk/kvalitetsit/hello/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideoLinkHandlerApplication {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(VideoLinkHandlerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
