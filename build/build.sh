270c5044bf2d4673d1fde1d4f2ec3ac2bae9ac3d
evtr <eva@kvalitetsit.dk>
2024-01-17 14:45:52 +0100

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -4,5 +4,6 @@
 docker build -t resources -f ./integrationtest/docker/Dockerfile-resources --no-cache ./integrationtest/docker
 docker run -d --name kithugs-resources resources
 
+
 # Build inside docker container
 docker run -v /var/run/docker.sock:/var/run/docker.sock  -v $HOME/.docker/config.json:/root/.docker/config.json -v $(pwd):/src -v $HOME/.m2:/root/.m2 --volumes-from kithugs-resources  maven:3-ibm-semeru-17-focal /src/build/maven.sh
