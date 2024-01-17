bcf005b04021310ce8daabca847d5226f6732a46
Jonas Pedersen <jpe@kvalitetsit.dk>
2024-01-17 15:04:01 +0100

    Upgrade to Java 21.

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Build inside docker container
-docker run -v /var/run/docker.sock:/var/run/docker.sock  -v $HOME/.docker/config.json:/root/.docker/config.json -v $(pwd):/src -v $HOME/.m2:/root/.m2 -v /tmp:/tmp  maven:3-ibm-semeru-17-focal /src/build/maven.sh
+docker run -v /var/run/docker.sock:/var/run/docker.sock  -v $HOME/.docker/config.json:/root/.docker/config.json -v $(pwd):/src -v $HOME/.m2:/root/.m2 --volumes-from kithugs-resources  maven:3-ibm-semeru-21-focal /src/build/maven.sh
